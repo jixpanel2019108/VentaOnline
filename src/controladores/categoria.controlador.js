16f8bdfb698f33a1685b7de6c514673c2e9e71dd
Jonatan <josuejonatan142@gmail.com>
2021-03-07 19:39:30 -0600

    Categoria Funciones Creation

---
+++ src/controladores/categoria.controlador.js
@@ -0,0 +1,67 @@
+'use strict'
+const { findByIdAndDelete } = require('../modelos/categoria.model');
+const Categoria = require('../modelos/categoria.model')
+
+function agregarCategoria(req,res){
+    var params = req.body
+    var categoriaModel = new Categoria();
+
+    if (req.user.rol != 'Administrador') return res.status(500).send({mensaje:'Solo los administradores pueden crear una categoria'})
+    if(!params.categoria) return res.status(500).send({mensaje:'No puede enviar campos vacios'})
+    categoriaModel.categoria = params.categoria;
+    Categoria.find({categoria:categoriaModel.categoria}).exec((err,usuarioEncontrado) => {
+        if (err) return res.status(500).send({mensaje:'Error en la peticion de usuario'})
+        if (usuarioEncontrado && usuarioEncontrado.length >= 1) return res.status({mensaje:'La categoria ya existe'})
+        categoriaModel.save((err, categoriaGuardada) => {
+            if(err) return res.status(500).send({mensaje:'Error en la peticion al servidor'})
+            if(categoriaGuardada){
+                return res.status(200).send(categoriaGuardada);
+            }else{
+                return res.status(500).send({mensaje:'Error al guardar usuario'});
+            }
+        })
+    })
+}
+
+function obtenerCategoria(req,res){
+    if (req.user.rol != 'Administrador') return res.status(500).send({mensaje:'Solo los administradores pueden crear una categoria'})
+    Categoria.find({},(err,categoriasEncontradas) =>{
+        if (err) return res.status(401).send({mensaje:'Error en la peticion de categorias'});
+        if (!categoriasEncontradas) return res.status(500).send({mensaje:'Error en la peticion'});
+
+        return res.status(200).send(categoriasEncontradas);
+    })
+}
+
+function editarCategoria(req,res){
+    var params = req.body;
+    var idCategoria = req.params.idCategoria;
+    var categoriaModel = new Categoria();
+
+    if (req.user.rol != 'Administrador') return res.status(500).send({mensaje:'Solo los administradores pueden crear una categoria'})
+    Categoria.findByIdAndUpdate(idCategoria,{categoria: params.categoria},{new:true, useFindAndModify: false},(err, categoriaModificada) => {
+        if(err) return res.status(500).send({mensaje:'Error en la peticion de categoria'});
+        if(!categoriaModel) return res.status(500).send({mensaje:'Error al editar categorias'});
+
+        return res.status(200).send({categoriaModificada}) 
+    })
+}
+
+function eliminarCategoria(req,res){
+    var idCategoria = req.params.idCategoria;
+    
+    if(req.user.rol != 'Administrador') return res.status(500).send({mensaje:'Solo los administradores pueden eliminar categorias'})
+    Categoria.findByIdAndDelete(idCategoria,(err, categoriaEliminada) => {
+        if (err) return res.status(500).send({mensaje:'Error en la peticion al servidor'});
+        if (!categoriaEliminada) return res.status(500).send({mensaje:'Error al enviar los datos al servidor'});
+
+        return res.status(200).send({CategoriaEliminada: categoriaEliminada});
+    })
+}
+
+module.exports = {
+    agregarCategoria,
+    obtenerCategoria,
+    editarCategoria,
+    eliminarCategoria
+}
--- src/rutas/categoria.rutas.js
+++ src/rutas/categoria.rutas.js
@@ -5,6 +5,10 @@ const categoriaControlador = require('../controladores/categoria.controlador')
 var md_autenticacion = require('../middlewares/authenticated');
 
 var api = express.Router();
+api.post('/agregarCategoria', md_autenticacion.ensureAuth, categoriaControlador.agregarCategoria);
+api.get('/obtenerCategoria', md_autenticacion.ensureAuth, categoriaControlador.obtenerCategoria);
+api.put('/editarCategoria/:idCategoria', md_autenticacion.ensureAuth, categoriaControlador.editarCategoria);
+api.delete('/eliminarCategoria/:idCategoria', md_autenticacion.ensureAuth, categoriaControlador.eliminarCategoria);
 
 
 module.exports = api;
